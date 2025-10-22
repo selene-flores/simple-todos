9c52b1ea28934792ee47d1555887c18806ca0ce7
selene-flores <122930419+selene-flores@users.noreply.github.com>
2025-10-21 21:55:09 -1000

    Update App.jsx

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from "./Task";
+
+const tasks = [
+    {_id: 1, text: 'Homework'},
+    {_id: 2, text: 'Laundry'},
+    {_id: 3, text: 'Meal Prep'}
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+
+      <ul>
+          { tasks.map(task => <Task key={task._id} task={ task } />) }
+      </ul>
   </div>
 );
